21f29797650593dc8278eb73f69b22655136cf05
Deeba Khan <deebakhan0208@gmail.com>
2022-12-23 11:52:24 +0530

    vercel

--- vidly/settings.py
+++ vidly/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ro3_0ix_bts_8&gsxz468kl&m+b7f7t^y$g6mr=pe2dvp7py+p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
